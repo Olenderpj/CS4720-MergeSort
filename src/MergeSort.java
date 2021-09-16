8755c29fb82fca4ae85148495a097cc1c7c8ef55
Olenderpj <Olenderpj1@gmail.com>
2021-09-16 11:28:53 -0600

    Small change to order of operations

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -5,8 +5,8 @@ public class MergeSort {
         int [] largeSampleArray = FileUtils.createIntArrayFromFile("TestFile.text");
 
         // calculates inversions with both the brute force method and the merge sort method.
-        Inversion inversion = new Inversion(largeSampleArray);
         System.out.println("Calculating inversions...");
+        Inversion inversion = new Inversion(largeSampleArray);
         System.out.println();
         System.out.printf("Brute Force Inversion Count: %d\n", inversion.getBruteForceInversionCount());
         System.out.printf("Merge Sort Inversion Count:  %d\n", inversion.getMergeSortInversionCount());
